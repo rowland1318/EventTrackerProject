ab518ee2d1cad2ccc3ec5e3940ce4e27b227b653
Rowland1318 <christina.a.rowland@gmail.com>
2020-04-10 16:26:08 -0600

    Add HikeTest; Create Boot Project

---
+++ Hiking/src/test/java/com/skilldistillery/hiking/HikingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.hiking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
