ffb37f5bec3243467178ee3104b464ce1143ba55
Rowland1318 <christina.a.rowland@gmail.com>
2020-04-10 15:45:28 -0600

    Create JPA project, stub Hikes

---
+++ JPAHiking/src/main/java/com/skilldistillery/hiking/entities/Hikes.java
@@ -0,0 +1,76 @@
+package com.skilldistillery.hiking.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Hikes {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Hikes(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Hikes() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Hikes [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hikes other = (Hikes) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
