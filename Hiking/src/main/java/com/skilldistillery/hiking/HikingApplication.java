7046e8e241ef99669dd10b235b8eb98aa58317d5
Rowland1318 <christina.a.rowland@gmail.com>
2020-04-13 08:38:29 -0600

    updated project for AWS

--- Hiking/src/main/java/com/skilldistillery/hiking/HikingApplication.java
+++ Hiking/src/main/java/com/skilldistillery/hiking/HikingApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.hiking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HikingApplication {
+public class HikingApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(HikingApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HikingApplication.class, args);
